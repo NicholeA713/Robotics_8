f6ce995f18184a3773ddc1b51132b389772a14e8
NicholeA713 <NicholeA713@stemcollegiate.ca>
2025-02-13 09:27:40 -0700

    Create Feb_13_2025.py

---
+++ Feb_13_2025.py
@@ -0,0 +1,5 @@
+For day 1, I learned about a new coding website called "Replit". It is a python based coding website, and I have already learned the basics! By adding print, brrackets, and two quotations allows a small text. With three quotations, it allows me to write a larger text, like a paragraph! Today's lesson was a great lesson, and I'm looking forward to the upcoming chllenges!
+print("Hello Neha :)")
+print ("Party Next Door")
+print("Frank Ocean")
+print("""I am signing up for Replit's 100 days of Python challenge!I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
