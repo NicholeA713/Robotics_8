166e45e064f663133e21951c01d3a752b3fd54bb
NicholeA713 <NicholeA713@stemcollegiate.ca>
2025-02-21 10:07:51 -0700

    Create Feb_20_2025.py

---
+++ Feb_20_2025.py
@@ -0,0 +1,9 @@
+Today, I learned how to input and paste each variable.
+food = ("Name a food: ")
+plant = input ("Name a type of plant: ")
+cooking = input ("Name a method of cooking: ")       
+food = input ("What word describes burned food?: ")
+DIY = input (" Name a DIY item: ")
+
+print (" MENU")
+print (food, "with", plant, "on a bed of", DIY) 
