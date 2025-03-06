bb6a7a5009147f19f49f31e4d96825fecb1ef2ff
NicholeA713 <NicholeA713@stemcollegiate.ca>
2025-03-06 09:28:39 -0700

    Create Mar_6_2025.py

---
+++ Mar_6_2025.py
@@ -0,0 +1,10 @@
+Today, I learned how to apply double equal signs, and put two strings together as a group, by indenting it.
+characterWhich = input ( """"Which character would you be in my world?A wizard, a knight, or a warrior? """)
+if characterWhich == "Knight":
+  print ("You are a knight, you are a brave and strong person")
+if characterWhich == "Wizard":
+  print ("You are a wizard, you are a wise and intelligent person")
+if characterWhich == "Warrior":
+  print ("You are a warrior, you are always in battle, mentally and physically")
+else:
+  print ("You are a peasant, didn't even choose one of my options. ")
