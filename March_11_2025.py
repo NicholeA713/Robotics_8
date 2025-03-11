aec0ca6f610cec2dd3c72ffc8a34f6264d495bc6
NicholeA713 <NicholeA713@stemcollegiate.ca>
2025-03-11 14:20:54 -0600

    Create March_11_2025.py

---
+++ March_11_2025.py
@@ -0,0 +1,13 @@
+# Replit Day 6
+Today, I learned how to use the code elif, which stands for "else if". I used it for when a user is inputting a code, it has to follow one of the three options I coded into the main.py.
+season = input("what is your favorite season?")
+if season == "spring":
+  print("Ah! The birds are chirping and flowers blooming.")
+elif season == "summer":
+  print("Catch some sun and cool off with a lemonade.")
+elif season == "autumn":
+  print("The leaves are changing and the air is crisp. Enjoy!")
+elif season == "winter":
+  print("Stay warm by the fire and watch the snow fall.")
+else: 
+  print("I don't know that season. Please try again.")
