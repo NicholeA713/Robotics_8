ce9f2b9fded732e9f97b888e0339761f7c27447c
NicholeA713 <NicholeA713@stemcollegiate.ca>
2025-03-13 09:05:21 -0600

    Create Mar_13_2025.py

---
+++ Mar_13_2025.py
@@ -0,0 +1,18 @@
+# Replit Day 7
+Today I learned how to use if statements properly for strings and different codes.
+order = input("What would you like to order: pizza or hamburger?")
+if order == "hamburger":
+  print("Thank you.")
+cheese = input("Do you want cheese?")
+if cheese == "yes":
+  print("You got it.")
+else: 
+  print("No cheese it is.")
+elif order == "pizza":
+  print("Pizza coming up.")
+  toppings = input("Do you want pepperoni on that?")
+if toppings:
+pings == "yes":
+  print("We will add pepperoni.")
+else:
+  print ("Your pizza will not have pepperoni.")
